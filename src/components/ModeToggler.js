ccba40c00e250217a18fe37936d268862a9aff27
ordazm <ordazing@gmail.com>
2022-10-01 19:09:20 -0300

    pc

--- src/components/ModeToggler.js
+++ src/components/ModeToggler.js
@@ -1,23 +1,23 @@
 function ModeToggler() {
-  let darkModeOn = true;
-  const darkMode = <h1>Dark Mode is On</h1>;
-  const lightMode = <h1>Light Mode is On</h1>;
+	let darkModeOn = true;
+	const darkMode = <h1>Dark Mode is On</h1>;
+	const lightMode = <h1>Light Mode is On</h1>;
 
-  function handleClick() {
-    darkModeOn = !darkModeOn;
-    if (darkModeOn === true) {
-      return console.log("DM is on");
-    } else {
-      return console.log("LM is on");
-    }
-  }
+	function handleClick() {
+		darkModeOn = !darkModeOn;
+		if (darkModeOn === true) {
+			return console.log("DM is on");
+		} else {
+			return console.log("LM is on");
+		}
+	}
 
-  return (
-    <div>
-      {darkModeOn ? darkMode : lightMode}
-      <button onClick={handleClick}>Click me</button>
-    </div>
-  );
+	return (
+		<div>
+			{darkModeOn ? darkMode : lightMode}
+			<button onClick={handleClick}>Click me</button>
+		</div>
+	);
 }
 
 export default ModeToggler;
