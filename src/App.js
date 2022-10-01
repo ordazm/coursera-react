49a698d8739e636314632fce4988a76de1dc7311
ordazm <ordazing@gmail.com>
2022-10-01 18:10:14 -0300

    act saturday

--- src/App.js
+++ src/App.js
@@ -6,25 +6,33 @@ import Intro1 from "./components/Intro1";
 import Intro2 from "./components/Intro2";
 import Intro3 from "./components/Intro3";
 import Footer from "./components/Footer";
+import Btn from "./components/Btn";
+import ModeToggler from "./components/ModeToggler";
+import Game from "./components/Game";
 
 function App() {
   return (
     <main>
-      <header className="App-header">
-        <img src={logo} className="App-logo"></img>
-        <Nav />
-      </header>
-      <section>
-        <Promo name="Michelle" lastName="Ordaz" />
-      </section>
-      <section>
-        <Intro1 />
-        <Intro2 />
-        <Intro3 />
-      </section>
-      <footer>
-        <Footer />
-      </footer>
+      <Game />
+      <ModeToggler />
+      <div>
+        <header className="App-header">
+          <img src={logo} className="App-logo"></img>
+          <Nav />
+        </header>
+        <section>
+          <Btn />
+          <Promo name="Michelle" lastName="Ordaz" />
+        </section>
+        <section>
+          <Intro1 />
+          <Intro2 />
+          <Intro3 />
+        </section>
+        <footer>
+          <Footer />
+        </footer>
+      </div>
     </main>
   );
 }
--- src/components/Btn.js
+++ src/components/Btn.js
@@ -1,3 +1,21 @@
-function Btn() {}
-return;
+// function Btn() {
+//   function clickHandler() {
+//     return console.log("clicked");
+//   }
+
+//   return <button onClick={clickHandler}>Click me</button>;
+// }
+// export default Btn;
+
+// /* const clickHandler = () =>
+//   console.log("clicked");
+// */
+
+function Btn() {
+  function clickHandler() {
+    return console.log("mouse over");
+  }
+
+  return <button onMouseOver={clickHandler}>Mouse Over</button>;
+}
 export default Btn;
